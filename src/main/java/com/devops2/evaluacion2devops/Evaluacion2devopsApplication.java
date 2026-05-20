0ca820efa2eb35d5227dbe8e5a4d3dc005842db6
lesdiazduoc <les.diazp@duocuc.cl>
2026-05-20 18:30:33 -0400

    Proyecto Spring Boot inicial

---
+++ src/main/java/com/devops2/evaluacion2devops/Evaluacion2devopsApplication.java
@@ -0,0 +1,13 @@
+package com.devops2.evaluacion2devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacion2devopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacion2devopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops2/evaluacion2devops/Evaluacion2devopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops2.evaluacion2devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion2devopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
